cbad4669835b9b78941994be98b53cdfabfa840a
artemkuzenkov <matt01@bk.ru>
2024-03-20 17:10:04 +0300

    addPR

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        System.out.println(catBean2.getMessage());
+        System.out.println(catBean == catBean2);
     }
 }
